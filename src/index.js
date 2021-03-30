ad1b6910535b2c30f8f404cbe7b492e61a527070
Bryanromerou <bryanromerou@gmail.com>
2021-03-30 10:49:31 -0700

    adds class based component solution

--- src/index.js
+++ src/index.js
@@ -10,47 +10,94 @@ const searchTerm = "art";
 const fetchUrl = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=50&api_key=${apiKey}`;
 
 
-const ArtApi = () => {
-	const [firstAPI, setFirstAPI] = useState(null);
-	const [secondAPI, setSecondAPI] = useState(null);
+// const ArtApi = () => {
+// 	const [firstAPI, setFirstAPI] = useState(null);
+// 	const [secondAPI, setSecondAPI] = useState(null);
 
-	const fetchFirstAPI = async() =>{
+// 	const fetchFirstAPI = async() =>{
+// 		const response = await axios.get('https://aws.random.cat/meow')
+// 		setFirstAPI(response.data.file)
+// 	}
+// 	const fetchSecondAPI = async() =>{
+// 		const response = await axios.get('https://randomfox.ca/floof/')
+// 		setSecondAPI(response.data.image)
+// 	}
+
+// 	useEffect(()=>{
+// 		fetchSecondAPI()
+// 		fetchFirstAPI()
+// 	},[])
+
+// 	const clickHandler = () =>{
+// 		setFirstAPI(null);
+// 		setSecondAPI(null);
+// 		fetchSecondAPI();
+// 		fetchFirstAPI();
+// 	}
+
+
+// 	return (
+// 		<div className="main">
+// 			<Quadrant image={firstAPI}/>
+// 			<Quadrant image={secondAPI}/>
+
+// 			<section className="section">
+// 				<Planet size={200} mood={(firstAPI && secondAPI) ? "blissful":"sad"} color="#FDA7DC" />
+// 			</section>
+// 			<section className="section">
+// 				<button onClick={clickHandler}>
+// 					Click me
+// 				</button>
+// 			</section>
+// 		</div>
+// 	);
+// }
+
+class ArtApi extends Component {
+	state = {
+		firstAPIData: [],
+		secondAPIData: [],
+	};
+
+	componentDidMount() {
+		this.fetchFirstAPI();
+		this.fetchSecondAPI();
+	}
+ 	fetchFirstAPI = async() =>{
 		const response = await axios.get('https://aws.random.cat/meow')
-		setFirstAPI(response.data.file)
+		this.setState({firstAPIData:response.data.file})
+		// setFirstAPI(response.data.file)
 	}
-	const fetchSecondAPI = async() =>{
+	fetchSecondAPI = async() =>{
 		const response = await axios.get('https://randomfox.ca/floof/')
-		setSecondAPI(response.data.image)
+		this.setState({secondAPIData:response.data.image})
+		// setSecondAPI(response.data.image)
+	}
+	clickHandler = () =>{
+		// this.setFirstAPI(null);
+		// this.setSecondAPI(null);
+		this.setState({firstAPIData:null,secondAPIData:null},()=>{
+			this.fetchFirstAPI();
+			this.fetchSecondAPI();
+		});
 	}
 
-	useEffect(()=>{
-		fetchSecondAPI()
-		fetchFirstAPI()
-	},[])
-
-	const clickHandler = () =>{
-		setFirstAPI(null);
-		setSecondAPI(null);
-		fetchSecondAPI();
-		fetchFirstAPI();
+	render() {
+		return (
+			<div className="main">
+ 			<Quadrant image={this.state.firstAPIData}/>
+ 			<Quadrant image={this.state.secondAPIData}/>
+ 			<section className="section">
+ 				<Planet size={200} mood={(this.state.firstAPIData && this.state.secondAPIData) ? "blissful":"sad"} color="#FDA7DC" />
+ 			</section>
+ 			<section className="section">
+ 				<button onClick={this.clickHandler}>
+ 					Click me
+ 				</button>
+ 			</section>
+ 		</div>
+		);
 	}
-
-
-	return (
-		<div className="main">
-			<Quadrant image={firstAPI}/>
-			<Quadrant image={secondAPI}/>
-
-			<section className="section">
-				<Planet size={200} mood={(firstAPI && secondAPI) ? "blissful":"sad"} color="#FDA7DC" />
-			</section>
-			<section className="section">
-				<button onClick={clickHandler}>
-					Click me
-				</button>
-			</section>
-		</div>
-	);
 }
 
 export default ArtApi;
